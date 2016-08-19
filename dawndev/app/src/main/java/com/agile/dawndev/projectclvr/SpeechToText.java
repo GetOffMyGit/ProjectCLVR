d7e1214f75bfb3c5d668c9f03707a0481913c291
Chahat Chawla <ccha504@aucklanduni.ac.nz>
2016-08-19 13:54:09 +1200

    With Farida: using android API - recording voice

--- dawndev/app/src/main/java/com/agile/dawndev/projectclvr/SpeechToText.java
+++ dawndev/app/src/main/java/com/agile/dawndev/projectclvr/SpeechToText.java
@@ -1,43 +1,78 @@
 package com.agile.dawndev.projectclvr;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.speech.RecognizerIntent;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class SpeechToText extends AppCompatActivity {
 
-    private Button record;
-    private TextView text;
+    protected static final int RESULT_SPEECH = 1;
+    private ImageButton record;
+    private TextView txtText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speech_to_text);
-        record = (Button) findViewById(R.id.record);
-        text = (TextView) findViewById(R.id.text);
-
-       final  int counter =0;
+        record = (ImageButton) findViewById(R.id.btnSpeak);
+        txtText = (TextView) findViewById(R.id.txtText);
 
         record.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Log.d("button", "clicked");
-                text.append(counter+"");
 
+                Intent intent = new Intent(
+                        RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+
+                try {
+                    startActivityForResult(intent, RESULT_SPEECH);
+                    txtText.setText("");
+                } catch (ActivityNotFoundException a) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Opps! Your device doesn't support Speech to Text",
+                            Toast.LENGTH_SHORT);
+                    t.show();
+                }
             }
         });
 
     }
-    public void goToMain(){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
 
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.activity_speech_to_text, menu);
+//        return true;
+//    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RESULT_SPEECH: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> text = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    txtText.setText(text.get(0));
+                }
+                break;
+            }
+
+        }
     }
 }
