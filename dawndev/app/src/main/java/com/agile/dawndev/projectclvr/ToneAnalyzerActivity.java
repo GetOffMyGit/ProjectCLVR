ac0721b81b861375ded623fffb883aa19706014c
zoercai <zoercai@gmail.com>
2016-08-26 15:24:23 +1200

    added saved output to save API calls

--- dawndev/app/src/main/java/com/agile/dawndev/projectclvr/ToneAnalyzerActivity.java
+++ dawndev/app/src/main/java/com/agile/dawndev/projectclvr/ToneAnalyzerActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,9 +18,10 @@ import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
 import org.w3c.dom.Text;
 
 public class ToneAnalyzerActivity extends AppCompatActivity implements ToneAnalyzerAsync.Tone {
-private ToneAnalyzer toneAnalyzerService;
+    private ToneAnalyzer toneAnalyzerService;
     private EditText inputEditText;
     private TextView outputTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +32,7 @@ private ToneAnalyzer toneAnalyzerService;
         toneAnalyzerService = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_05_19);
         toneAnalyzerService.setUsernameAndPassword("345d437c-b0d0-4f07-8b0e-3a5bb21a4931", "qsWAYzFipvWy");
         inputEditText = (EditText) findViewById(R.id.inputText_et);
-        inputEditText.setText( "I know the times are difficult! Our sales have been "
+        inputEditText.setText("I know the times are difficult! Our sales have been "
                 + "disappointing for the past three quarters for our data analytics "
                 + "product suite. We have a competitive data analytics product "
                 + "suite in the industry. But we need to do our job selling it! "
@@ -42,16 +44,14 @@ private ToneAnalyzer toneAnalyzerService;
                 + "business outcomes. Economy has nothing to do with it.");
         outputTextView = (TextView) findViewById(R.id.outputTextView);
 
-Button analyzeTextButton = (Button) findViewById(R.id.analyze_text_button);
+        Button analyzeTextButton = (Button) findViewById(R.id.analyze_text_button);
         outputTextView.setMovementMethod(new ScrollingMovementMethod());
-
-
 
         analyzeTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String text = inputEditText.getText().toString();
-                new ToneAnalyzerAsync().execute( toneAnalyzerService,text,outputTextView);
+                new ToneAnalyzerAsync().execute(toneAnalyzerService, text, outputTextView);
             }
         });
 
