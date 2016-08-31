a99cc4fa563fe514838973f5dbf6ba5c9762a3dc
eliza <elizaorchard@gmail.com>
2016-09-01 11:50:34 +1200

    Updated the radar graph

--- dawndev/app/src/main/java/com/agile/dawndev/projectclvr/ToneAnalyzerGraph.java
+++ dawndev/app/src/main/java/com/agile/dawndev/projectclvr/ToneAnalyzerGraph.java
@@ -6,62 +6,113 @@ import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.RadarChart;
 
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 
 import com.github.mikephil.charting.data.RadarData;
 import com.github.mikephil.charting.data.RadarDataSet;
 import com.github.mikephil.charting.data.RadarEntry;
-
+import com.github.mikephil.charting.formatter.AxisValueFormatter;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ToneAnalyzerGraph extends AppCompatActivity {
-    private RadarChart mChart;
-    private JSONObject jsonObj;
-    private JSONObject loudScreaming;
-    private JSONArray loudScreaminng;
+    private RadarChart emotionChart;
+    private RadarChart languageChart;
+    private RadarChart socialChart;
+
+    private JSONArray emotionTones;
+    private JSONArray languageTones;
+    private  JSONArray socialTones;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tone_analyzer_graph);
 
-        mChart = (RadarChart) findViewById(R.id.chart1);
+        emotionChart = (RadarChart) findViewById(R.id.emotionGraph);
+        languageChart = (RadarChart) findViewById(R.id.languageGraph);
+        socialChart = (RadarChart) findViewById(R.id.socialToneGraph);
+        String[]emotionLabels = new String[]{"Anger", "Disgust", "Fear", "Joy", "Sadness"};
 
+        String[] languageToneLabels = new String[]{"Analytical", "Confident", "Tentative"};
+        String[] socialToneLabels = new String[]{"Openness", "Conscientiousness", "Extraversion", "Agreeableness", "Emotional Range"};
         try {
-             jsonObj = new JSONObject(Constants.ToneAnalyzerResult).getJSONObject("document_tone");
+            JSONObject reader = new JSONObject(Constants.ToneAnalyzerResult);
+
+            // Emotion Tone Graph
+            JSONArray emotionToneCategories  = reader.getJSONArray("tone_categories");
+             emotionTones = emotionToneCategories.getJSONObject(0).getJSONArray("tones");
+            // Emotion Tone Graph
+              JSONArray languageToneCategories  = reader.getJSONArray("tone_categories");
+             languageTones = languageToneCategories.getJSONObject(1).getJSONArray("tones");
+            // Social Tone Graph
+            JSONArray socialToneCategories  = reader.getJSONArray("tone_categories");
+             socialTones = languageToneCategories.getJSONObject(2).getJSONArray("tones");
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        try {
-             loudScreaming = jsonObj.getJSONObject("document_tone");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        try {
-             loudScreaminng = loudScreaming.getJSONArray("tone_categories");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
 
+setUpGraph(emotionChart,emotionTones,emotionLabels);
+        setUpGraph(languageChart,languageTones,languageToneLabels);
+        setUpGraph(socialChart,socialTones,socialToneLabels);
 
+    }
+    public void setUpGraph(RadarChart chart, JSONArray array,String[] labels){
         List<RadarEntry> entries = new ArrayList<RadarEntry>();
 
-      //  for (YourData data : dataObjects) {
+//entries.add(new RadarEntry(emotionTones.get(0)))
 
-            // turn your data into Entry objects
-            entries.add(new RadarEntry((float)8.0,(float)9.0));
-        entries.add(new RadarEntry((float)1.0,(float)3.0));
-        entries.add(new RadarEntry((float)5.0,(float)5.0));
-        RadarDataSet dataSet = new RadarDataSet(entries, "Label"); // add entries to dataset
-//        dataSet.setColor(...);
-//        dataSet.setValueTextColor(...);
+        for ( int i = 0; i<array.length();i++){
+            try {
+                entries.add(new RadarEntry(Float.parseFloat(array.getJSONObject(i).get("score").toString()) ,array.getJSONObject(0).get("tone_name")));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        RadarDataSet dataSet = new RadarDataSet(entries, "Tone Analyzer"); // add entries to dataset
+
+        dataSet.setColor(Color.rgb(103, 110, 129));
+        dataSet.setFillColor(Color.rgb(103, 110, 129));
+        dataSet.setDrawFilled(true);
+        dataSet.setFillAlpha(180);
+        dataSet.setLineWidth(2f);
+        dataSet.setDrawHighlightCircleEnabled(true);
+        dataSet.setDrawHighlightIndicators(false);
+
         RadarData radarData = new RadarData(dataSet);
-        mChart.setData(radarData);
-        mChart.invalidate();
-        //  }
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setTextSize(9f);
+        xAxis.setYOffset(0f);
+        xAxis.setXOffset(0f);
+        xAxis.setValueFormatter(new AxisValueFormatter() {
+
+            private String[] mActivities = new String[]{"Anger", "Disgust", "Fear", "Joy", "Sadness"};
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return mActivities[(int) value % mActivities.length];
+            }
+
+            @Override
+            public int getDecimalDigits() {
+                return 0;
+            }
+        });
+
+        chart.setData(radarData);
+        chart.setDescription("");
+        chart.invalidate();
     }
 }
+
