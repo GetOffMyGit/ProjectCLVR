6b40d12ba38f41becb2806c93d42720c31a40def
Paul Hojun Joo <hjoo488@aucklanduni.ac.nz>
2016-10-03 20:06:10 +1300

    Pair programmed with Nikhil Prasad, npra508. Fixed bug with null company email.

--- dawndev/app/src/main/java/com/agile/dawndev/projectclvr/SpeechAnalyserActivity.java
+++ dawndev/app/src/main/java/com/agile/dawndev/projectclvr/SpeechAnalyserActivity.java
@@ -133,6 +133,7 @@ public class SpeechAnalyserActivity extends Activity {
             mCompanyKey = (String) savedInstanceState.getSerializable("companyKey");
             mTestKey = (String) savedInstanceState.getSerializable("testKey");
             mCompanyName = (String) savedInstanceState.getSerializable("companyName");
+            mCompanyEmail = (String) savedInstanceState.getSerializable("companyEmail");
         }
 
 
